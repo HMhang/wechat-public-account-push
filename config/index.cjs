d0077fcd9cf5a4c63b97143f309844bb8ca239f2
HMhang <160763418+HMhang@users.noreply.github.com>
2024-02-22 03:10:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyvLO6cfCKM8FS_t6OyxPpErEalo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'xqNOuVlLtBCnGtMvtAooTdwdizJfTQouoJsCyndOvYw',
+      useTemplateId: '8KB_apcVIutrJX5UBLCXynnS62XS5K6s3yGLpLRaGmk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-28',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'xqNOuVlLtBCnGtMvtAooTdwdizJfTQouoJsCyndOvYw',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyvLO6cfCKM8FS_t6OyxPpErEalo',
+      id: '',
     }
   ],
 
